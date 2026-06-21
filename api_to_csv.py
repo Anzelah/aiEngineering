75c3b84d21d4bfb4021758e0f15f5b149e2257ce
Anzelah <jepkoechanzellah@gmail.com>
2026-06-21 23:40:07 +0300

    check the instance of data type returned

--- api_to_csv.py
+++ api_to_csv.py
@@ -11,7 +11,8 @@ def pull_data():
 
 
     try:
-        res = requests.get("url", timeout=5)
+        url = "https://jsonplaceholder.typicode.com/users"
+        res = requests.get(url, timeout=5)
         res.raise_for_status()
         return res.json()
     except requests.exceptions.HTTPError as http_err:
@@ -31,10 +32,11 @@ def write_to_csv():
     if not data:
         return None
     
+    if not isinstance(data, list):
+        print(f"Unexpected data format: ", type(data))
+        return
+    
     with open("output.csv", "w", newline='') as csv_file:
-        fieldnames = ["Name", "Age", "Occupation"]
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
-        for row in data:
-            writer.writerow(row)
+        writer = csv.writer(csv_file, delimiter=' ')
+        writer.writerows(data)
     
