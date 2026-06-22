9c9fac1d100ebed205ecc63a63faf05a19162ea3
Anzelah <jepkoechanzellah@gmail.com>
2026-06-22 21:59:05 +0300

    add environmental variables for testing

--- load_env.py
+++ load_env.py
@@ -3,5 +3,16 @@ This function script loads secrets from an
 .env file and handles error cleanly
 """
 
+from dotenv import load_dotenv
+import os
+
 def load_secrets():
-    pass
+    """A file that loads .env file"""
+    if not os.path.exists(".env"):
+        print("The .env file doesn't exist")
+        return
+    
+    load_dotenv()
+    api_key = os.getenv("API_KEY")
+
+    
