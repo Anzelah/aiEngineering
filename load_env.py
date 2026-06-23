0325e86ae1d8a5e53202c02bb732c0ebf2a08e17
Anzelah <jepkoechanzellah@gmail.com>
2026-06-23 12:32:55 +0300

    edit code for readability

--- load_env.py
+++ load_env.py
@@ -20,7 +20,7 @@ def load_secrets():
             return None
         
         for key, value in config.items():
-            print(f"These are the environmental variables {key}: {value}")
+            print(f"{key}: {value}")
         return config
 
     except OSError as os_err:
