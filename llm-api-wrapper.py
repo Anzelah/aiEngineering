a99a929e60ff4d7f01f1f8a983183ddcfc2d9671
Anzelah <jepkoechanzellah@gmail.com>
2026-07-03 13:29:01 +0300

    completed cli parsing

--- llm-api-wrapper.py
+++ llm-api-wrapper.py
@@ -3,9 +3,25 @@ Build an LLM API wrapper with command line interface.
 This wrapper is a support simple text analyzer. We'll do the support ticket analyzer later'
 """
 
-def user_input():
+import argparse
 
-    pass
+
+def user_input():
+    """Parse user input from CLI using Argparse"""
+    parser = argparse.ArgumentParser(description='Take user input text and passes in onto another function', suggest_on_error=True)
+    parser.add_argument('user_question', type=str, help='The string to send to api')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose output')
+    args = parser.parse_args()
+
+    # Check for empty strings
+    if not args.user_question.strip():
+        parser.error(f"Input cannot be empty")
+    
+    if args.verbose:
+        print(f"User input received succesfully")
+
+    return args.user_question
+
 
 def call_api():
     pass
