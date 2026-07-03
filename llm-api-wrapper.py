6b5989ddd99cab2ce0b2ee606b0ee70f351a97a9
Anzelah <jepkoechanzellah@gmail.com>
2026-07-03 11:25:46 +0300

    project structure

--- llm-api-wrapper.py
+++ llm-api-wrapper.py
@@ -1,2 +1,26 @@
 """
-Build an LLM API wrapper with command line interface"""
+Build an LLM API wrapper with command line interface.
+This wrapper is a support simple text analyzer. We'll do the support ticket analyzer later'
+"""
+
+def user_input():
+
+    pass
+
+def call_api():
+    pass
+
+
+def json_output():
+    """Enforce JSON response format via the prompt"""
+    pass
+
+
+def errors_and_rate_limit():
+    pass
+
+
+
+def log_results():
+    pass
+
